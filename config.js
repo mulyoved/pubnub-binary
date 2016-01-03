0443037e1d6519531b5cb89a6db6acc69be74171
mulyoved <mulyoved@gmail.com>
2016-01-03 19:01:55 +0200

    spelling

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 exports.pubSubSetup = {
   pubsub: 'pubnub',
-  room_send: 'test2:send',
-  room_recv: 'test2:recv',
+  room_send: 'test3:send',
+  room_recv: 'test3:recv',
   pubnub: {
-    publish_key: 'pub-c-c24684b2-f532-4925-8666-56e6f5255795',
-    subscribe_key: 'sub-c-3996d2a2-a8e9-11e5-a64b-0619f8945a4f'
+    publish_key: 'pub-c-replace with your key',
+    subscribe_key: 'sub-c-replace with your key'
   }
 };
